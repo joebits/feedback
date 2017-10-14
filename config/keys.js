19de36dc550ea71c0b18126f6b4eac26ba9eeebc
Joel Jönsson <85joel@gmail.com>
2017-10-14 14:06:44 +0200

    Added heroku as a trusted proxy for passport

--- config/keys.js
+++ config/keys.js
@@ -3,5 +3,6 @@ if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
     //development
+    console.log('------- You are in the development environment --------')
     module.exports = require('./dev');
 }
